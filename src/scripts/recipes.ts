02b4c7f565751b73c2e27568d1fd24778b5f5a6f
norviah <jmasresha@gmail.com>
2020-06-18 22:11:58 -0700

    feat: merged recipes into their relevant item

---
+++ src/scripts/recipes.ts
@@ -0,0 +1,28 @@
+import { writeFileSync } from 'fs';
+
+import { files, category } from '../util/category';
+import { directories } from '../util/directories';
+import { get } from '../util/get';
+
+// Each recipe has the property 'craftedItemInternalId', which represents the
+// internal ID of the item the recipe represents, so we'll use that property to
+// set recipes for items.
+
+const recipes: obj[] = category('Recipes', directories.sanitized);
+
+for (const file of files('Items', directories.sanitized)) {
+  const items: obj[] = get(file);
+
+  for (const item of items) {
+    // Remember that some items have variations, and if they do, the internal
+    // IDs are in the array, so we'll have to check for this possibility.
+    const id: number = item.internalId ?? item.variations[0].internalId;
+
+    // Try to find the recipe for this item.
+    const recipe: obj | undefined = recipes.find((recipe: any) => recipe.craftedItemInternalId === id);
+
+    item.recipe = recipe ?? null;
+  }
+
+  writeFileSync(file, JSON.stringify(items, null, 2));
+}
