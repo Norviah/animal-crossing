341cd2b9a1bfa55e3d8032d6497270d07393a355
Norviah <21983700+Norviah@users.noreply.github.com>
2020-07-05 06:40:20 -0700

    style(json): alphabetized output

--- src/util/write.ts
+++ src/util/write.ts
@@ -2,13 +2,20 @@ import { writeFileSync } from 'fs';
 import beautify from 'json-beautify';
 
 /**
+ * Sorts an array based off of the name of each element.
+ */
+function sort(a: any, b: any): number {
+  return (a.name ?? a.english ?? a.event) > (b.name ?? b.english ?? b.event) ? 1 : -1;
+}
+
+/**
  * Writes the given data to the given path, using the beautify module.
  * @param path  The path to save the data to.
  * @param data  The data to save.
  * @param data  Represents the maximum fixed character width.
  */
-function write(path: string, data: any[], width?: number): void {
-  writeFileSync(path, beautify(data, (<unknown>null) as any[], 2, width ?? 80));
+function write(path: string, data: obj[], width?: number): void {
+  writeFileSync(path, beautify(data.sort(sort), (<unknown>null) as any[], 2, width ?? 80));
 }
 
 export { write };
