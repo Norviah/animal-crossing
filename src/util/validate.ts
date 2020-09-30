7246cf68247c990177c7438c495ab0d168e82611
norviah <jmasresha@gmail.com>
2020-09-30 15:58:35 -0700

    refactor: refactor codebase

--- src/util/validate.ts
+++ src/util/validate.ts
@@ -1,9 +1,18 @@
 /**
- * Deletes any values from the given object that evaluates to a falsy value.
- * @param object The object to validate.
+ * Returns a copy of the given object with falsy values removed.
+ * @param  object The object to validate.
+ * @return        The given object copied with no falsy values.
  */
-function validate(object: obj): void {
-  for (const key in object) if (!object[key]) delete object[key];
-}
+export function validate(object: obj): obj {
+  // Initialize a new array referencing the keys from the given object filtered
+  // to remove any key that results to a non falsy value.
+  const keys: string[] = Object.keys(object).filter((key: string) => object[key]);
 
-export { validate };
+  const result: obj = {};
+
+  for (const key of keys) {
+    result[key] = object[key];
+  }
+
+  return result;
+}
